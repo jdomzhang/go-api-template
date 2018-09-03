0be24cdbfe6e2a52f4168d93cc54699a32f78ffb
Steven Zhang <jdomzhang@gmail.com>
2018-09-03 16:08:20 +0800

    change static folder structure

--- template/static/js/about.js
+++ template/static/js/about.js
@@ -1,3 +1,11 @@
 function onClickMe() {
-  alert('You clicked me!')
+  var xhr = new XMLHttpRequest()
+  xhr.open("POST", '/api/v1/say/hello', true)
+  
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      alert('Server: ' + this.response)
+    }
+  }
+  xhr.send()
 }
