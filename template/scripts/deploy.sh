9e22b94141280778915cde5f7599bd662569679e
Steven Zhang <jdomzhang@gmail.com>
2018-10-24 20:17:37 +0800

    fix path issue in deploy script

--- template/scripts/deploy.sh
+++ template/scripts/deploy.sh
@@ -1,6 +1,6 @@
 mkdir -p /usr/share/websites/{{name}}
 systemctl stop {{name}}
-tar --overwrite --overwrite-dir -C /usr/share/websites/{{name}} -xvzf ./{{name}}.tar.gz
+tar --overwrite --overwrite-dir -C /usr/share/websites/{{name}} -xvzf ~/upload/{{name}}/{{name}}.tar.gz
 # rm /usr/share/websites/{{name}}/*.tar
 chmod 755 /usr/share/websites/{{name}}/{{name}}
 #PORT=3009 /usr/share/websites/{{name}}/{{name}}
