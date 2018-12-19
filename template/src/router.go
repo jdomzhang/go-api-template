fb80f5dc720f2def22295aafb53a65546f67d72c
Steven Zhang <jdomzhang@gmail.com>
2018-12-19 10:38:22 +0800

    simplify api route

--- template/src/router.go
+++ template/src/router.go
@@ -4,29 +4,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Route contains all the API mapping
 func Route() *gin.Engine {
 	r := gin.Default()
 
 	r.GET("/", SayHello)
-	r.GET("/api", SayHello)
-	r.GET("/api/v1", SayHello)
-	r.Static("/api/v1/static", "./static")
 	r.StaticFile("/favicon.ico", "./static/img/favicon.ico")
 
-	// r.Static("/userdata", "./userdata")
-
-	// Simple group: v1
-	{
-		v1 := r.Group("/api/v1")
-
-		{
-			say := v1.Group("/say")
-
-			// temp
-			say.POST("/hello", SayHello)
-		}
-
-	}
+	r.GET("/api", SayHello)
+	r.GET("/api/v1", SayHello)
+	r.GET("/api/v1/hello", SayHello)
+	r.Static("/api/v1/static", "./static")
 
 	return r
 }
