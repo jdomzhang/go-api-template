687a8eca5db66aad3269ee0c22660da88e3b3866
Steven Zhang <jdomzhang@gmail.com>
2019-02-26 02:24:15 +0800

    fixed issue - service not starting after reboot

--- template/scripts/install.sh
+++ template/scripts/install.sh
@@ -1,8 +1,8 @@
 systemctl stop {{name}}
-unlink /etc/systemd/system/{{name}}.service
+unlink /etc/systemd/system/multi-user.target.wants/{{name}}.service
 chmod 755 /usr/share/websites/{{name}}/{{name}}
 mkdir -p /usr/share/websites/{{name}}
 cp ~/upload/{{name}}/scripts/api.service /usr/share/websites/{{name}}/{{name}}.service
-ln -s /usr/share/websites/{{name}}/{{name}}.service /etc/systemd/system/{{name}}.service
+ln -s /usr/share/websites/{{name}}/{{name}}.service /etc/systemd/system/multi-user.target.wants/{{name}}.service
 systemctl daemon-reload
 systemctl restart nginx
