ec8639a2e6ce1f58bb6d9c9038bc08d8870ea5f1
Steven Zhang <jdomzhang@gmail.com>
2019-03-27 16:20:40 +0800

    change controller file names

--- template/src/controllers/ctrl.hello.go
+++ template/src/controllers/ctrl.hello.go
@@ -6,5 +6,5 @@ import (
 
 // SayHello is a simple method to check if the API is working.
 func SayHello(c *gin.Context) {
-	c.String(200, "Hello API")
+	c.String(200, fmt.Sprintf("[%s]Hello API\n", time.Now().Format(time.RFC3339)))
 }
